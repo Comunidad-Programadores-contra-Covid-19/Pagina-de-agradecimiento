e6e1a6c4e6482111b320476c238b3dc65c6ef91c
Fluzko <fluzko@gmail.com>
2020-05-02 14:49:21 -0300

    index page welcome desing + donate fix

--- src/models/index.js
+++ src/models/index.js
@@ -10,11 +10,6 @@ const db = {};
 const initdb = require('../config/dbInit')
 
 let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 sequelize = initdb;
 
 fs
