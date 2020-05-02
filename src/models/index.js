2070dbdb84442684449826663d6514771570d3ba
Fluzko <fluzko@gmail.com>
2020-05-02 12:58:34 -0300

    fix  sequelize index.js

--- src/models/index.js
+++ src/models/index.js
@@ -7,13 +7,15 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/dbConfig.js')[env];
 const db = {};
+const initdb = require('../config/dbInit')
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+sequelize = initdb;
 
 fs
   .readdirSync(__dirname)
