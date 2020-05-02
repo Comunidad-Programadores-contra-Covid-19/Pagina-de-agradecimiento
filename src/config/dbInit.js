1fe15f0301405bbcafb4683f3c4f67e6caabe179
Fluzko <fluzko@gmail.com>
2020-05-02 12:13:40 -0300

    db logging change

--- src/config/dbInit.js
+++ src/config/dbInit.js
@@ -8,6 +8,7 @@ if(process.env.DATABASE_URL){
     module.exports = new Sequelize(process.env.DATABASE_URL,{
         dialect: "postgres",
         protocol: "postgres",
+        logging: true,
     });
 }else{
     module.exports = new Sequelize(
