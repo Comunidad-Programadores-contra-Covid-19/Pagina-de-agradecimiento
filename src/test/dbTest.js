da2e1cc3eea43ed42b963216d3c286f12e1e36d8
JulianAtlas <julianatlasovich@gmail.com>
2020-05-02 22:29:24 -0300

    harcoded posts

--- src/test/dbTest.js
+++ src/test/dbTest.js
@@ -1,10 +1,12 @@
 const db = require('../config/dbInit');
 const Post = require('../models').Post;
+fs = require('fs');
+
 
 db.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
   .catch(err => console.error('Unable to connect to the database:', err));
-
+/*
 try {
   Post.update(
     { test: 'Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets cont' },
@@ -15,4 +17,13 @@ try {
   })
 } catch (err) {
   handleError(err)
-}
+}*/
+
+Post.findAll({raw : true}).then(posts =>{
+  fs.writeFile('text.json', JSON.stringify(posts),  function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  })
+})
--- src/test/requester.js
+++ src/test/requester.js
@@ -4,4 +4,7 @@ post('http://localhost:8000/twitter/byID',data={'id':'1255205007802216449'})
 '/twitter/byQuery'
 post('http://localhost:8000/twitter/byQuery',data={'query':'gracias medicos'})
 
+
+'/twitter/byQuery'
+post('https://teagradezco.herokuapp.com/twitter/byQuery',data={'query':'gracias medicos'})
  
